87a7545177d6ec8a51c625e9433c92307495d905
Alireza <alirezaaraby5@gmail.com>
2023-03-31 06:51:22 -0700

    menu added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./dist/*.{html,js}"],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: {
+          50: '#fdf8f6',
+          100: '#f2e8e5',
+          200: '#eaddd7',
+          300: '#e0cec7',
+          400: '#d2bab0',
+          500: '#bfa094',
+          600: '#a18072',
+          700: '#007990',
+          800: '#846358',
+          900: '#43302b',
+        },
+      }
+    },
   },
   plugins: [],
 }
